15a2f5deb95c57a558d6193d80e83c7470080cef
budaman <budamantvydas@gmail.com>
2017-12-25 13:50:14 +0200

    added player API and prop match

---
+++ src/components/FullRoster.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FullRoster = () => {
+  return <div>FullRoster is working is working</div>;
+};
+
+export default FullRoster;
--- src/components/Roster.js
+++ src/components/Roster.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import FullRoster from "./FullRoster";
+import Player from "./Player";
 
 const Roster = () => {
-  return <div>Roster is working</div>;
+  return (
+    <div>
+      <h2>This is roster page!</h2>
+      <Switch>
+        <Route exact path="/roster" component={FullRoster} />
+        <Route path="/roster/:number" component={Player} />
+      </Switch>
+    </div>
+  );
 };
 
 export default Roster;
